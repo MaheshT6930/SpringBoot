a6bc553bcdc582062ec47896a648e2ee80ed2403
Mahesh Tadepalli <mtadepalli98@gmail.com>
2025-09-10 01:44:57 -0500

    Initial commit from javaFullStackTraining

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student(101,"SACHIN","TENDULKAR","20-5/A, EAST, MUMBAI");
+        System.out.println(student1);
+
+
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,61 @@
+package org.example;
+
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public Student(){
+
+    }
+
+    public Student(int studentId, String firstName, String lastName, String address) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
